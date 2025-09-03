60369097204e656ec725a072f91db159a2d7b614
christopher-ling <stu.christopher.ling@gmail.com>
2025-09-02 23:00:43 -0400

    rm docker folder

--- Docker files/run.bat
+++
@@ -1,2 +0,0 @@
-@echo OFF
-docker run --rm -it --cpus 4 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --name="cs2200" -v "%CD%\workspace":/cs2200 -w="/cs2200" systemsandnetworks/cs2200-students
